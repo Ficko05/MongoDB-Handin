4b3f3f97e18245088357e88591d85fcee59a3e43
Ficko05 <filip-f@hotmail.com>
2022-03-21 16:33:30 +0100

    testers

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ const testmoduel = require("./models/Test")
 app.use(express.json())
 
 //mongoose.connect('mongodb+srv://new-user:admin123@cluster0.g7szi.mongodb.net/ass-3?retryWrites=true&w=majority',{
-//mongoose.connect('mongodb://localhost:27017/m',{
-mongoose.connect('mongodb://localhost:26061/mydb',{
+mongoose.connect('mongodb://localhost:27017/twitter',{
+//mongoose.connect('mongodb://localhost:26061/mydb',{
     useNewUrlParser: true,
 }) 
 
 
 app.get('/', async(req, res) => {
-    const tester = new testmoduel({fname: "test", lname: "test"})
+    const tester = new testmoduel({first_name: "test", last_name: "test"})
 
     try{
         await tester.save()
