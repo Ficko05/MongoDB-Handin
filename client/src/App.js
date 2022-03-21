d0a982f06b1f3592a17262a02aff6cc3039d5338
Nur Salem <Nur.Salem.Sabet@fellowmind.dk>
2022-03-21 13:47:59 +0100

    Frontend init

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import './App.css';
+import NameForm from './form';
+
 function App() {
   return (
     <div className="App">
-      
-       <p>hello</p>
-      
+      <header className="App-header">
+      <img src={"https://draywilliams.com/wp-content/uploads/2020/05/imageedit_56_5278557772.png"} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <NameForm/>
+      </header>
     </div>
   );
 }
